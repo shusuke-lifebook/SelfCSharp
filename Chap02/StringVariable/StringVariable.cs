ebf337042c5c1c3821336953ce1f63e040be4562
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-03-14 09:18:18 +0900

    文字列への式展開のサンプルを追加

---
+++ Chap02/StringVariable/StringVariable.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SelfCSharp.Chap02;
+
+public class StringVariable
+{
+    static void Main(string[] args)
+    {
+        string name = "山田";
+        Console.WriteLine($"こんにちは{name}さん!");
+    }
+}
